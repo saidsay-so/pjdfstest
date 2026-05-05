01db21a14c0bb72c1d90d19a1bdfd24d4e552137
asomers <asomers@users.noreply.github.com>
2026-05-05 20:14:00 +0000

    deploy: 1e287d3e4fbc45c7c3ed990a3ffdf3390e2b02ab

--- doc/trait.impl/gumdrop/trait.Options.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["pjdfstest",[["impl Options for <a class=\"struct\" href=\"pjdfstest/struct.ArgOptions.html\" title=\"struct pjdfstest::ArgOptions\">ArgOptions</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[154]}
