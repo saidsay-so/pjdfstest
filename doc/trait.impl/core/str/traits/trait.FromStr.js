cc784b3ecfc3a99c9fc5c1133fb1cc24a664a706
asomers <asomers@users.noreply.github.com>
2026-05-30 21:23:21 +0000

    deploy: 1bb1cf3ad9a1506a0baa70a2822ea7950b53a9b6

--- doc/trait.impl/core/str/traits/trait.FromStr.js
+++ doc/trait.impl/core/str/traits/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["pjdfstest",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"pjdfstest/flags/enum.FileFlags.html\" title=\"enum pjdfstest::flags::FileFlags\">FileFlags</a>",0]]]]);
+    const implementors = Object.fromEntries([["pjdfstest",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"pjdfstest/flags/enum.FileFlags.html\" title=\"enum pjdfstest::flags::FileFlags\">FileFlags</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
